b7e6043bd563dcaa867c53ac520fa3b4f64dd0e7
Sivan Mehta <Sivanmehta2@gmail.com>
2018-10-15 21:47:46 -0700

    there are 2 svgs

--- graph.js
+++ graph.js
@@ -2,6 +2,8 @@ const daysInYear = 25;
 const trials = 1000;
 const scalex = 200;
 
+const store = Array(daysInYear).fill([0]);
+
 // generate a set for a given size of group
 // and then return the number of unique birthdays
 function singleTrial(groupSize) {
@@ -28,40 +30,39 @@ function generateData() {
     .map((_, i) => ({ value: singleObservation(trials, i), idx: i}))
 }
 
-function scaffold() {
-  const blanks = Array(daysInYear).fill(0).map((_, i) => i);
-
-  d3.select('#graph')
-    .selectAll('rect')
-    .data(blanks).enter()
-    .append('rect')
-    .attr('height', 30)
-    .attr('x', 0)
-    .attr('y', d => d * 30)
-    .attr('fill', 'black');
-
-  d3.select('#graph')
-    .selectAll('text')
-    .data(blanks).enter()
-    .append('text')
-    .attr('y', d => d * 30 + 20)
-    .attr('fill', 'black');
-
-  redraw();
-}
-
-function redraw() {
+function redrawOne() {
   const observations = generateData();
 
-  d3.selectAll('rect')
+  d3.select('#single')
+    .selectAll('rect')
     .attr('width', d => observations[d].value * scalex)
     .attr('fill', d => observations[d].value > .5 ? 'green' : 'red');
 
-  d3.selectAll('text')
+  d3.select('#single')
+    .selectAll('text')
     .text(d => observations[d].value)
     .attr('x', d => observations[d].value * scalex + 30)
     .attr('fill', d => observations[d].value > .5 ? 'green' : 'red');
 }
 
-scaffold();
-setInterval(redraw, 500);
+function scaffold(selector) {
+  const blanks = Array(daysInYear).fill(0).map((_, i) => i);
+
+  d3.select(selector)
+    .selectAll('rect')
+    .data(blanks).enter()
+    .append('rect')
+    .attr('height', 30)
+    .attr('x', 0)
+    .attr('y', d => d * 30);
+
+  d3.select(selector)
+    .selectAll('text')
+    .data(blanks).enter()
+    .append('text')
+    .attr('y', d => d * 30 + 20);
+}
+
+scaffold('#single');
+scaffold('#overall');
+setInterval(redrawOne, 500);
